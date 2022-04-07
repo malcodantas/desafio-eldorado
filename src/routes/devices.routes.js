f6b59bf942ced73c7a187d498f46fcf9faed9ea4
Malco Dantas <malcodantas18@gmail.com>
2022-04-06 22:06:05 -0300

    refactor: organizando arquivo de rotas dos devices

--- src/routes/devices.routes.js
+++ src/routes/devices.routes.js
@@ -1,28 +1,18 @@
 
 const { Router } = require('express')
-const DeviceRepository = require('../repositories/DeviceRepository')
-const deviceRoutes = Router()
 const { DeviceController } = require('../controllers/DeviceController')
-const resolver = (handlerFn) => {
-  return (req, res, next) => {
-    return Promise.resolve(handlerFn(req, res, next))
-      .catch(e => next(e))
-  }
-}
+const resolver = require('../shared/utils/Resolver')
 const deviceController = new DeviceController()
+const deviceRoutes = Router()
 
 deviceRoutes.get('/', resolver(deviceController.list))
 
-deviceRoutes.get('/:id', async (request, response) => {
-  const deviceRepository = new DeviceRepository()
-  const id = request.params.id
-  const deviceObj = await deviceRepository.getById(id)
-  response.json(deviceObj)
-})
-
 deviceRoutes.post('/', resolver(deviceController.create))
+
+deviceRoutes.get('/:id', resolver(deviceController.findOne))
 
 deviceRoutes.put('/:id', resolver(deviceController.update))
 
 deviceRoutes.delete('/:id', deviceController.delete)
+
 module.exports = { deviceRoutes }
