654a49446b027904a411b1055bdd8b826850d4bb
Malco Dantas <malcodantas18@gmail.com>
2022-04-20 22:52:34 -0300

    chore: voltando a utilizar a porta 3000 como padrão

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { routes } = require('./routes')
 const ErrorHandler = require('./shared/middleware/ErrorHandler')
-const PORT = 3001
+const PORT = 3000
 
 const sequelize = require('./database')
 
