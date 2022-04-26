f26feffc32d3b930ca469207d49dbf059f90747d
Malco Dantas <malcodantas18@gmail.com>
2022-04-25 23:44:22 -0300

    feat: configurações gerais

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,60 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoryCreateComponent } from './components/category/category-create/category-create.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+
+// Angular extra imports
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
+
+
+// Material Imports
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list';
+import {MatCard, MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input';
+import { CategoryCrudComponent } from './views/category-crud/category-crud.component';
+import { CategoryReadComponent } from './components/category/category-read/category-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryCreateComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    CategoryCrudComponent,
+    CategoryReadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
