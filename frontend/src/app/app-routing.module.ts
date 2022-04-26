dcde726dd29b3cfbe0444bd8449e9ff090a1a474
Malco Dantas <malcodantas18@gmail.com>
2022-04-25 23:38:48 -0300

    feat: criando estrutura de rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CategoryCreateComponent } from './components/category/category-create/category-create.component';
+import { CategoryCrudComponent } from './views/category-crud/category-crud.component';
+import { AuthGuard } from './guards/auth.guard';
+const routes: Routes = [
+  {
+    path:'category',
+    component: CategoryCrudComponent
+    // canActivate:[AuthGuard]
+  },
+  {
+    path:'category/create',
+    component:CategoryCreateComponent
+    // canActivate:[AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
