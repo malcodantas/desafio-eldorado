7481c94638d86fc547cd09718276253c702ca90f
Malco Dantas <malcodantas18@gmail.com>
2022-04-25 23:40:06 -0300

    feat: adicionando dependencia para tratar o CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { routes } = require('./routes')
 const ErrorHandler = require('./shared/middleware/ErrorHandler')
 const AuthHandler = require('./shared/middleware/AuthHandler')
+const cors = require('cors')
 const PORT = 3000
 const { userRoutes } = require('./routes/user.routes')
 
@@ -15,6 +16,7 @@ sequelize.authenticate().then(() => {
 })
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/login', userRoutes)
 app.use(AuthHandler)
