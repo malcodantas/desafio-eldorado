769e8f2c8a5369cf619ddb7ee0f669241b4ccb27
Malco Dantas <malcodantas18@gmail.com>
2022-04-25 23:43:07 -0300

    feat: iniciando o desenvolvimento do service da entidade category

---
+++ frontend/src/app/services/category.service.ts
@@ -0,0 +1,18 @@
+import { apiResponse } from './../models/apiRespose.model';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar'
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  baseUrl='http://localhost:3000/category'
+  constructor(private snackBar:MatSnackBar,private http:HttpClient) { }
+  
+  list():Observable<apiResponse>{
+    return this.http.get<apiResponse>(this.baseUrl)
+  }
+}
