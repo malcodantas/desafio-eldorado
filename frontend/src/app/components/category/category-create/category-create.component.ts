d9a3bfe27f335291dda50672b325fbf43cad13c6
Malco Dantas <malcodantas18@gmail.com>
2022-04-27 20:24:11 -0300

    feat: enviando requisição para criar category

--- frontend/src/app/components/category/category-create/category-create.component.ts
+++ frontend/src/app/components/category/category-create/category-create.component.ts
@@ -1,5 +1,7 @@
+import { Category } from './../../../models/category.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category-create',
@@ -7,13 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./category-create.component.css']
 })
 export class CategoryCreateComponent implements OnInit {
-
-  constructor(private router:Router) { }
+  category:Category = {name:''}
+  constructor(private router:Router,private modelService:CategoryService) { }
 
   ngOnInit(): void {
   }
 
   cancelOperation(){
     this.router.navigate(['/category'])
+  }
+
+  createCategory(){
+    this.modelService.create(this.category).subscribe((response)=>{
+        //TODO: Verify statuscode 
+        console.log('category created:',response)
+        this.router.navigate(['/category'])
+    })
   }
 }
--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -1,3 +1,4 @@
+import { Category } from './../models/category.model';
 import { apiResponse } from './../models/apiRespose.model';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -14,5 +15,9 @@ export class CategoryService {
   
   list():Observable<apiResponse>{
     return this.http.get<apiResponse>(this.baseUrl)
+  }
+  
+  create(category:Category):Observable<Category>{
+    return this.http.post<Category>(this.baseUrl,category)
   }
 }
