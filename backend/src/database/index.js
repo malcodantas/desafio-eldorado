0f2e6c3a5572c1aa983e48d5483b438042e997d4
Malco Dantas <malcodantas18@gmail.com>
2022-04-28 23:57:28 -0300

    fix: corrigindo chamada da variável de ambiente DB_HOST

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.DB_HOST,
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   pool: {
     max: 5,
